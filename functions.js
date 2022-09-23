8b18f8ad8bdd2eac2608fead00058211e94d4fe5
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-09-22 23:50:16 -0700

    Update the function practice exercises

--- functions.js
+++ functions.js
@@ -1,13 +1,48 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+function printGreeting() {
+  console.log("Hi! Welcome to Turing!");
+}
+  printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
-
+function booksToRead(genre) {
+  console.log(`This afternoon I will read a ${genre} book.`);
+}
+  booksToRead("Western");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+//
 
+// I tried this a few time and was able to able to get this to print the company name or
+// write a separate code for the range, but not all three.
+//
+function companyPay(companyName, maxNum, minNum) {
+  var companyName = "Moonpies";
+  var diff = maxNum - minNum;
+
+  console.log(`Wow, with ${companyName} being such a delicious name for a company, no wonder the pay range is ${diff}!`);
+ }
+companyPay(110000, 90000);
 
 // 4: Write a function that satifies the following interaction pattern:
+
+//  I tried multiple ways to figure this one out to satisfy the two Parameters
+// in your example, but I could not get it to run.
+
+function checkStock(amount, item) {
+  var amount = "";
+  var item = "";
+  if (`${amount} === 4 && ${item} == Coffee`) {
+      console.log("Coffee is stocked");
+  } else if (`${amount} === 3 || ${item} === Tortillas`) {
+    console.log("Tortillas-running LOW");
+  } else if (`${amount} === 2 || ${item} === Cheese`) {
+    console.log("Cheese - OUT of stock!");
+  } else if (`${amount} === 1 || ${item} === Salsa`) {
+  }
+
+ checkStock();
+
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,14 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+    I am trying to get better about not spending as much time trying to figure out
+    and make perfect all assignments, without stopping and reaching out for help.
+
 - How would you explain to a 5 year old what a function is?
+    I would explain that a function is something that happens again and again. Like when you're done playing with your block and you put them in the bucket over and over again until you stop.
+
 - Share your understanding of arguments and parameters.
+    Parameters are like drafting a great team of fantasy football players, but unless I set and lock in my line up , the argument, I will never win.
+
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+    I really struggles with multiple parameters when they are a mix of "strings" numbers.  I am not sure how to set up a correct argument.
